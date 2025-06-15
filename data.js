ab1c3e78e52f92f35652236981e8cc60fa42bcc5
lerni82 <lerni82@gmail.com>
2025-06-15 12:22:32 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,293 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-hts-udvar1",
+      "name": "Hátsó udvar1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.011621314743049282,
+        "pitch": 0.05966260080113628,
+        "fov": 0.879058642673606
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3063198381213628,
+          "pitch": 0.012535275320875172,
+          "rotation": 4.71238898038469,
+          "target": "1-hts-udvar2"
+        },
+        {
+          "yaw": 1.1987965579177207,
+          "pitch": 0.007136473311794234,
+          "rotation": 4.71238898038469,
+          "target": "2-hts-udvar-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-hts-udvar2",
+      "name": "Hátsó udvar2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.000004025435044852088,
+        "pitch": 0,
+        "fov": 0.879058642673606
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.38428937972249244,
+          "pitch": 0.05643136597833198,
+          "rotation": 0,
+          "target": "0-hts-udvar1"
+        },
+        {
+          "yaw": 0.5501472612470515,
+          "pitch": 0.049237308215159814,
+          "rotation": 0,
+          "target": "2-hts-udvar-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-hts-udvar-3",
+      "name": "Hátsó udvar 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.11026763685124585,
+        "pitch": 0.019799179281239532,
+        "fov": 0.879058642673606
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0724165276354327,
+          "pitch": 0.05196348459199562,
+          "rotation": 6.283185307179586,
+          "target": "0-hts-udvar1"
+        },
+        {
+          "yaw": -1.8040796975497848,
+          "pitch": 0.11271125467563081,
+          "rotation": 6.283185307179586,
+          "target": "1-hts-udvar2"
+        },
+        {
+          "yaw": 0.6697843418074569,
+          "pitch": 0.10617760623374295,
+          "rotation": 0,
+          "target": "3-oldal-kert"
+        },
+        {
+          "yaw": 0.8314107185283213,
+          "pitch": 0.03294702362704882,
+          "rotation": 4.71238898038469,
+          "target": "4-utcafront"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-oldal-kert",
+      "name": "Oldal kert",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 0.879058642673606
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4306366963925541,
+          "pitch": 0.01105178972086307,
+          "rotation": 1.5707963267948966,
+          "target": "1-hts-udvar2"
+        },
+        {
+          "yaw": -1.004534256952386,
+          "pitch": 0.15869233844197694,
+          "rotation": 7.0685834705770345,
+          "target": "2-hts-udvar-3"
+        },
+        {
+          "yaw": 2.418879849251523,
+          "pitch": 0.01640877255104556,
+          "rotation": 4.71238898038469,
+          "target": "4-utcafront"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-utcafront",
+      "name": "Utcafront",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 0.879058642673606
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9566252877356369,
+          "pitch": 0.03503585669648501,
+          "rotation": 12.566370614359176,
+          "target": "5-utcafront-bejrat"
+        },
+        {
+          "yaw": -0.5888982284831403,
+          "pitch": 0.09974960068181105,
+          "rotation": 6.283185307179586,
+          "target": "3-oldal-kert"
+        },
+        {
+          "yaw": -0.7041652382051886,
+          "pitch": 0.028472110734606204,
+          "rotation": 1.5707963267948966,
+          "target": "2-hts-udvar-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-utcafront-bejrat",
+      "name": "Utcafront bejárat",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.04528520831203586,
+        "pitch": 0.03820555813209836,
+        "fov": 0.879058642673606
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4983372837812894,
+          "pitch": 0.08208199242648462,
+          "rotation": 1.5707963267948966,
+          "target": "4-utcafront"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
